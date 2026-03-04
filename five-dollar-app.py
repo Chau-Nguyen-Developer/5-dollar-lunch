397274896175aef090bc2d7248742fb2a4479edb
Chau Nguyen Developer (Christina) <chau.nguyen.dev@gmail.com>
2026-03-04 01:10:52 +0000

    Will fix CSS elements on front page later

--- five-dollar-app.py
+++ five-dollar-app.py
@@ -37,27 +37,34 @@ def welcome():
 data = [
     {
         "name" : "Spiral Notebook",
-        "price": 3.39
+        "price": 3.39,
+        "image_url": "https://img-1.kwcdn.com/product/open/34273584381743a98021073c1a1c725b-goods.jpeg?imageView2/2/w/264/q/70/format/avif"
     },
     {
         "name": "Monthly Calendar",
-        "price": 6.99
+        "price": 6.99,
+        "image_url": "https://img.kwcdn.com/product/open/55fee14820b34bb0b51f08bbcddc447b-goods.jpeg?imageView2/2/w/800/q/70/format/avif"
+
     },
     {
         "name": "Papermate Pens",
-        "price": 4.89
+        "price": 4.89,
+        "image_url": "https://target.scene7.com/is/image/Target/GUEST_adb0937c-9c7d-4665-8012-5ad39de12a81?wid=750&qlt=80"
     },
     {
         "name": "Post-it Sticky Notes",
-        "price": 10.99
+        "price": 10.99,
+        "image_url": "https://target.scene7.com/is/image/Target/GUEST_bcd05de0-9e4b-4e3b-b351-87d30e317379?wid=600&hei=600&qlt=80"
     },
     {
         "name": "Letter-Size Paper",
-        "price": 5.79
+        "price": 5.79,
+        "image_url": "https://target.scene7.com/is/image/Target/GUEST_1b8f976b-2072-422d-95f3-d747532a8de5?wid=600&hei=600&qlt=80"
     },
     {
         "name": "Paper folder",
-        "price": 1.89
+        "price": 1.89,
+        "image_url": "https://target.scene7.com/is/image/Target/GUEST_6b59a3bf-02bd-4442-aa55-c0cb0f4d6c54?wid=600&hei=600&qlt=80"
     },
 ]
 @app.route('/search/<budget>')
